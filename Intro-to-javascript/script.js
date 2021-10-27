7c3f50e96db9812140c663e22c84575aa4a88565
YaxinJiang <yj2513@columbia.edu>
2021-10-27 15:17:00 -0400

    javascript

---
+++ Intro-to-javascript/script.js
@@ -0,0 +1,23 @@
+// (command+/) Comment out a line
+// alert("Hello World!")
+
+function sayhello() {
+    console.log("Hello World!")
+}
+
+sayhello();
+
+// ++ represents x=x+1
+// -- represents x=x-1
+
+let age=25;
+let height=5;
+console.log(age,height)
+
+let noun = "cat";
+let adj = "up";
+let verb = "photograph";
+
+let sentence= `My ${noun} leaps ${adj} when I ${verb} a rainbow in the sky.`
+console.log(sentence);
+
