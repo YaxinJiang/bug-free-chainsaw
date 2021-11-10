b2f9e0d3e85460bf8e0c527390da9854f132d01e
YaxinJiang <yj2513@columbia.edu>
2021-11-09 20:43:32 -0500

    java in class

---
+++ Intro-to-javascript/script2.js
@@ -0,0 +1,11 @@
+let age=12;
+let height=5;
+
+if (age<8){
+    console.log ("check out the Merry");
+}else if (age>8 && age<65 && height>4.5){
+    console.log ("check out the middle");
+}else{
+    console.log ("check out the up");
+}
+
